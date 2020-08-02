02d4db6944b6c4cd3704762ac3cf8c88c79a4641
Vinicius Velasco <viniciusvelasco@DESKTOP-T1DOPLV.localdomain>
2020-08-02 11:25:45 -0400

    Commit inicial

---
+++ app/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '5aea014644ab1d6684c4954d310aa40d'),
+    },
+  },
+});
---
+++ app/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'c886f14a-c4c8-4d1f-809e-d9912d20718f'
+};
